f66a9c691469d3b5eed724f0d57819ed7ac8c288
Magdalena Wawryniuk <mwawryniuk01@gmail.com>
2026-01-18 15:07:20 +0100

    Added to database a few record into table 'posts'. Added display of post on main forum page with limited post content lenght.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,20 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i [ show edit update destroy ]
   def index
-    @users = User.all
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  private
+  # Function finding post with selected ID. This method is called in all method listed in 'before action' in order to reduce writing the same code many times
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Function for checking if post we are trying to created meets our requirements before making query to database
+  def post_params
+    params.expect(post: [ :title, :content, :user_id ] )
   end
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ApplicationRecord
   belongs_to :user
   has_many :comments, dependent: :destroy
 
-  validates :title, :content, presence: true
+  validates :title, :content, :user_id, presence: true
 end
